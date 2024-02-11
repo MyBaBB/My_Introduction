9ec58e88b1556333483fe14a4cf081ff977a59d5
Brett Baker <GitBaBB@MyBaBB.com>
2024-02-11 06:22:00 -0800

    added image zoom on click

--- src/components/TopRightImage.jsx
+++ src/components/TopRightImage.jsx
@@ -1,18 +1,46 @@
-// eslint-disable-next-line no-unused-vars
-import React from 'react'
-import phoneInHands from '../img/phoneInHands.webp'
-import Tours from '../img/Tours400x300.webp'
-const TopRightImage = () => {
-  return (
-    <div>
-      <div > 
-        
-        <img src={phoneInHands} alt="phoneInHands" width={400}  className="m-1 my-2 z-2 rounded-xl shadow-xl " />
-        <img src={Tours} alt="Tours" width={400}  className="m-1 mt-2 z-2 rounded-xl shadow-xl "/>
-      </div>
-    </div>
-  )
-}
 
+        // eslint-disable-next-line no-unused-vars
+        import React, { useState } from 'react';
+        import phoneInHands from '../img/phoneInHands.webp';
+        import Tours from '../img/Tours400x300.webp';
+        import Beach from '../img/BeachRentals.png';
+
+        const TopRightImage = () => {
+          const [isZoomed, setIsZoomed] = useState(false);
+
+          const handleImageClick = () => {
+            setIsZoomed(!isZoomed);
+          };
+
+          return (
+            <div>
+              <div>
+                <img
+                  src={phoneInHands}
+                  alt="phoneInHands"
+                  width={isZoomed ? 800 : 400}
+                  className={`m-1 my-2 z-2 rounded-xl shadow-xl ${isZoomed ? 'zoomed' : ''}`}
+                  onClick={handleImageClick}
+                />
+                <img
+                  src={Tours}
+                  alt="Tours"
+                  width={isZoomed ? 800 : 400}
+                  className={`m-1 mt-2 z-2 rounded-xl shadow-xl ${isZoomed ? 'zoomed' : ''}`}
+                  onClick={handleImageClick}
+                />
+                <img
+                  src={Beach}
+                  alt="Beach"
+                  width={isZoomed ? 800 : 400}
+                  className={`m-1 mt-2 z-2 rounded-xl shadow-xl ${isZoomed ? 'zoomed' : ''}`}
+                  onClick={handleImageClick}
+                />
+              </div>
+            </div>
+          );
+        };
+
+     
 export default TopRightImage
 
