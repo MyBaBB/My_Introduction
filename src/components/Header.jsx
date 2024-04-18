d26fc373bf60ed38b12ad3193e32bcf8b0f679ad
Brett Baker <GitBaBB@MyBaBB.com>
2024-04-18 06:20:53 -0700

    cleaned up the header now to add hamburger menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
-import CssLogo from "../img/cssLogoHeight80px.png";
-import MeImg from "./MeImg";
+// import CssLogo from "../img/cssLogoHeight80px.png";
+import NewMe from "../img/NewMe.png";
 import ScubaMask from "./ScubaNavFolder/ScubaMask";
 import ContactButton from "./ContactButton";
 import EmailButton from "./EmailButton";
@@ -9,15 +9,24 @@ const header = () => {
   return (
     <div>
       
-      <header
-        className="reheader 
-        text-[2rem] sm:text-[2.5rem] lg:text-[3rem] "
-      >
-        
-        <h1 className="reheaderh1 ">Brett Baker</h1>
-        <img src={CssLogo} alt="CssLogo" className="reheaderimg" />
-        <MeImg />
+      <header className="header relative flex items-center h-[100px] z-50 w-screen bg-[#24558d]
+        text-[2rem] sm:text-[2.5rem] lg:text-[3rem]  "
+      > 
+         
+      <img src={NewMe} alt="New Me" width={80} height={80} className="absolute top-2 left-2"  />
+   
+    
        
+        <div className="m-auto w-fit  -translate-x-4 ">
+        <h1 className=" font-Caprasimo text-center   text-amber-300 ">Brett Baker</h1>
+        </div>
+       
+       
+   
+ 
+  
+  
+ 
 
         
         
