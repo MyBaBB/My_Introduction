6b151e2d1322421990f5df6aa18a2ceb1c771a10
Brett Baker <GitBaBB@MyBaBB.com>
2024-10-07 03:24:33 -0700

    testing chunks build in vite confige

--- src/components/EyeBallFolder/EyeBall.jsx
+++ src/components/EyeBallFolder/EyeBall.jsx
@@ -7,7 +7,7 @@ import './Eyeball.css';
 
 
 
-
+ 
 
 
 
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,17 @@
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   base: './',
   plugins: [react()],
-
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+        },
+      },
+    },
+  },
 })
