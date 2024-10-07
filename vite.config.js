11d400431ded3ee792413cb72fe2c35c3ea268c7
Brett Baker <GitBaBB@MyBaBB.com>
2024-10-07 04:41:40 -0700

    removed errors from build see notes,  added chunksize limit in viteconfig and  commented out the eval command in node module

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,12 @@
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   base: './',
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom'],
-        },
-      },
-    },
-  },
+ build: {
+    chunkSizeWarningLimit: 1600,
+  } 
+
 })
