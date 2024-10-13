6f6717237718e2c016060ab2c84fd3865e3a7031
Brett Baker <GitBaBB@MyBaBB.com>
2024-10-13 11:46:12 -0700

    done

--- src/components/EmailButton.jsx
+++ src/components/EmailButton.jsx
@@ -1,12 +1,17 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
 import { BiMailSend } from "react-icons/bi";
+
+const goBack = () => {
+  window.history.back();
+};
+
 const EmailButton = () => {
   return (
     <div>
-       <a href="https://mybabb.com/#contact" target="_blank" rel="noreferrer">
+       <a href="https://contact.mybabb.com" >
       <div className="relative flex justify-center items-center z-[19]" >
-        <button
+        <button onClick={goBack}
           className=" emailMe mr-12 rounded-lg     
          border-2 border-solid border-black bg-black/50 p-2 text-lg text-blue-400
          hover:bg-black hover:shadow-lg hover:text-white "
